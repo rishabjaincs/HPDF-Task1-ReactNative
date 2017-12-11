3257a90554a3728b6c9050cf7853350aa3c1f020
rishabjaincs <31016370+rishabjaincs@users.noreply.github.com>
2017-12-11 20:03:18 +0530

    Add files via upload

--- screen/tab4.js
+++ screen/tab4.js
@@ -6,7 +6,7 @@ import {Text,Button,Image,View} from 'react-native';
 import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
 export default class tab4 extends React.Component{
   static navigationOptions={
-    tabBarLabel: 'Tab4',
+    tabBarLabel: 'Mail',
     tabBarIcon: ({ tintColor }) => 
       <Image
         source={{uri:'https://qph.ec.quoracdn.net/main-thumb-106720517-200-kenemtfjcvnmelsgvldvxfywrheijala.jpeg'}}
@@ -26,7 +26,7 @@ export default class tab4 extends React.Component{
 style={{width: 120, height: 120, marginTop:30, marginBottom:30, alignItems: 'center'}}
 source={{uri:'https://abs.twimg.com/icons/apple-touch-icon-192x192.png'}}/>
     <Text style={{fontSize: 30, color: '#55ACEE'}}>
-    Moments
+    Inbox
     </Text>
         </View>  
     }}
